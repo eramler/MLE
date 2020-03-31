b1361f691a11fed25a99fc8f9446fbd06f01ad95
eramler <56918234+eramler@users.noreply.github.com>
2020-03-31 20:18:56 +0100

    Add files via upload

--- class_leastsquares.py
+++ class_leastsquares.py
@@ -48,12 +48,17 @@ class LeastSquares(class_ngarch.NGARCH):
         #print(self.R_stdevs)
 
     def squared_residuals(self):
-        self.sqresiduals = numpy.zeros((self.n_randparams, self.run_steps))
+        self.sqres_means = numpy.zeros((self.n_randparams, self.run_steps))
+        self.sqres_Rfast = numpy.zeros((self.n_randparams, self.run_steps))
         for j in range(0, self.run_steps):
-            self.sqresiduals[:,j] = (self.R_means[:,j] - self.pair.data[j])**2
+            self.sqres_means[:,j] = (self.R_means[:,j] - self.pair.data[j])**2
+            self.sqres_Rfast[:,j] = (self.Rfast_values[:,j] - self.pair.data[j])**2
             for i in range(0, self.n_randparams): 
-                if self.sqresiduals[i,j] > 1000:
-                    self.sqresiduals[i,j] = 1000
+                if self.sqres_means[i,j] > 1000:
+                    self.sqres_means[i,j] = 1000
+                if self.sqres_Rfast[i,j] > 1000:
+                    self.sqres_Rfast[i,j] = 1000
+        self.Rfast_sqres_min_index = numpy.argmin(self.sqres_Rfast[:,3])
         #print(self.sqresiduals)
 
     def optimise_Rfast(self):
@@ -64,17 +69,28 @@ class LeastSquares(class_ngarch.NGARCH):
         self.deltas = self.randparams[:,3]
         self.omegas = self.randparams[:,4]
         self.Rfast_values = numpy.zeros((self.n_randparams, self.run_steps))
-        bin_params = numpy.zeros(21)
-        bin_params[0] = 0.5
-        self.calc_Rfast(3, bin_params)
 
-    def calc_Rfast(self, bin_i, bin_params):
+        bin_params = numpy.zeros(21)
+        bin_param_step = numpy.ones(21)
+        bin_param_names = ['v_c','v_A','v_B','v_G','v_D','v_O','v_AA','v_AB','v_AG','v_AD','v_AO','v_BB','v_BG','v_BD','v_BO','v_GG','v_GD','v_GO','v_DD','v_DO','v_OO']
+        self.bin_i = 3
+        #self.calc_Rfast(3, bin_params)
+        #m = iminuit.Minuit(self.calc_Rfast, forced_parameters=bin_param_names, use_array_call=True)
+        m = iminuit.Minuit.from_array_func(self.calc_Rfast, bin_params, error=bin_param_step, name=bin_param_names)
+        for i in range(0, self.run_steps):
+            self.bin_i = i
+            m.migrad()
+            print(m.fval)
+            print(m.values)
+        print(self.Rfast_values)
+
+    def calc_Rfast(self, bin_params):
         #Setting up the parameters to be optimised in the 'fast' simulation
         #A: Alpha, B: Beta, G: Gamma, D: Delta, O: Omega, c: constant
         v_c,v_A,v_B,v_G,v_D,v_O,v_AA,v_AB,v_AG,v_AD,v_AO,v_BB,v_BG,v_BD,v_BO,v_GG,v_GD,v_GO,v_DD,v_DO,v_OO = bin_params
 
         #calculate Rfast values (multiline sum)
-        self.Rfast_values[:,bin_i] = v_c + \
+        self.Rfast_values[:,self.bin_i] = v_c + \
                             self.alphas * (v_A + v_AA*self.alphas + v_AB*self.betas + v_AG*self.gammas + v_AD*self.deltas + v_AO*self.omegas) + \
                             self.betas * (v_B + v_BB*self.betas + v_BG*self.gammas + v_BD*self.deltas + v_BO*self.omegas) + \
                             self.gammas * (v_G + v_GG*self.gammas + v_GD*self.deltas + v_GO*self.omegas) + \
@@ -83,15 +99,11 @@ class LeastSquares(class_ngarch.NGARCH):
         
         Rfast_chivalues = numpy.zeros(self.n_randparams)
         for k in range(0, self.n_randparams):
-            Rfast_chivalues[k] = ((self.Rfast_values[k,bin_i] - self.R_means[k,bin_i]) / self.R_stdevs[k,bin_i]) ** 2 / self.n_randparams
-        print(Rfast_chivalues)
+            Rfast_chivalues[k] = ((self.Rfast_values[k,self.bin_i] - self.R_means[k,self.bin_i]) / self.R_stdevs[k,self.bin_i]) ** 2 / self.n_randparams
+        #print(Rfast_chivalues)
         Rfast_totalchi = numpy.sum(Rfast_chivalues)
-        print(Rfast_totalchi)
+        #print(Rfast_totalchi)
         return Rfast_totalchi
-        
-
-        
-
 
 
     def __init__(self, pair, n_randparams=10, n_simulations=10, run_steps='MAX'):
@@ -107,42 +119,105 @@ class LeastSquares(class_ngarch.NGARCH):
         
         self.rand_params()
         self.run_simulations()
-        self.squared_residuals()
 
         self.optimise_Rfast()
 
+        self.squared_residuals()
+
 
 pair = class_currencypair.CurrencyPair('EURGBP')
-x = LeastSquares(pair, n_randparams=10, n_simulations=10, run_steps=5)
+x = LeastSquares(pair, n_randparams=1000, n_simulations=100, run_steps=5)
 
 pyplot.figure()
-pyplot.plot(x.randparams[:,0], x.sqresiduals[:,4], linestyle='', marker='o')
+pyplot.plot(x.randparams[:,0], x.Rfast_values[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,0], x.R_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,0], x.Rfast_values[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
 pyplot.xlabel('alpha value')
-pyplot.ylabel(r'$(R_{mean} - R_{data})^2$')
+pyplot.ylabel('R Value')
 #pyplot.show()
 
 pyplot.figure()
-pyplot.plot(x.randparams[:,1], x.sqresiduals[:,4], linestyle='', marker='o')
+pyplot.plot(x.randparams[:,1], x.Rfast_values[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,1], x.R_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,1], x.Rfast_values[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
 pyplot.xlabel('beta value')
-pyplot.ylabel(r'$(R_{mean} - R_{data})^2$')
+pyplot.ylabel('R Value')
 #pyplot.show()
 
 pyplot.figure()
-pyplot.plot(x.randparams[:,2], x.sqresiduals[:,4], linestyle='', marker='o')
+pyplot.plot(x.randparams[:,2], x.Rfast_values[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,2], x.R_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,2], x.Rfast_values[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
 pyplot.xlabel('gamma value')
-pyplot.ylabel(r'$(R_{mean} - R_{data})^2$')
+pyplot.ylabel('R Value')
 #pyplot.show()
 
 pyplot.figure()
-pyplot.plot(x.randparams[:,3], x.sqresiduals[:,4], linestyle='', marker='o')
+pyplot.plot(x.randparams[:,3], x.Rfast_values[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,3], x.R_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,3], x.Rfast_values[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
 pyplot.xlabel('delta value')
-pyplot.ylabel(r'$(R_{mean} - R_{data})^2$')
+pyplot.ylabel('R Value')
 #pyplot.show()
 
 pyplot.figure()
-pyplot.plot(x.randparams[:,4], x.sqresiduals[:,4], linestyle='', marker='o')
+pyplot.plot(x.randparams[:,4], x.Rfast_values[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,4], x.R_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,4], x.Rfast_values[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
 pyplot.xlabel('omega value')
-pyplot.ylabel(r'$(R_{mean} - R_{data})^2$')
+pyplot.ylabel('R Value')
+pyplot.show()
+
+##################################PLOTTING SQUARED RESIDUALS###################################
+
+pyplot.figure()
+pyplot.plot(x.randparams[:,0], x.sqres_Rfast[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,0], x.sqres_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,0], x.sqres_Rfast[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
+pyplot.xlabel('alpha value')
+pyplot.ylabel(r'$(R_{sim} - R_{data})^2$')
+#pyplot.show()
+
+pyplot.figure()
+pyplot.plot(x.randparams[:,1], x.sqres_Rfast[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,1], x.sqres_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,1], x.sqres_Rfast[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
+pyplot.xlabel('beta value')
+pyplot.ylabel(r'$(R_{sim} - R_{data})^2$')
+#pyplot.show()
+
+pyplot.figure()
+pyplot.plot(x.randparams[:,2], x.sqres_Rfast[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,2], x.sqres_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,2], x.sqres_Rfast[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
+pyplot.xlabel('gamma value')
+pyplot.ylabel(r'$(R_{sim} - R_{data})^2$')
+#pyplot.show()
+
+pyplot.figure()
+pyplot.plot(x.randparams[:,3], x.sqres_Rfast[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,3], x.sqres_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,3], x.sqres_Rfast[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
+pyplot.xlabel('delta value')
+pyplot.ylabel(r'$(R_{sim} - R_{data})^2$')
+#pyplot.show()
+
+pyplot.figure()
+pyplot.plot(x.randparams[:,4], x.sqres_Rfast[:,3], label='Rfast values', linestyle='', marker='o')
+#pyplot.plot(x.randparams[:,4], x.sqres_means[:,3], label='Mean R values',linestyle='', marker='o')
+pyplot.plot(x.randparams[x.Rfast_sqres_min_index,4], x.sqres_Rfast[x.Rfast_sqres_min_index,3], label='Minimum SqRes Value', linestyle='', marker='o')
+pyplot.legend()
+pyplot.xlabel('omega value')
+pyplot.ylabel(r'(R_{sim} - R_{data})^2')
 pyplot.show()
 
 
